e5854b38f335f61f70bd23b160baa728ff2d4911
iiieq <93109189+iiieq@users.noreply.github.com>
2022-12-13 17:45:33 -0500

    Removed clutter

--- src/main/java/com/jagrosh/jmusicbot/commands/music/PlayCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/music/PlayCmd.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2016 John Grosh <john.a.grosh@gmail.com>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.jagrosh.jmusicbot.commands.music;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
@@ -35,10 +21,7 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.exceptions.PermissionException;
 
-/**
- *
- * @author John Grosh <john.a.grosh@gmail.com>
- */
+
 public class PlayCmd extends MusicCommand
 {
     private final static String LOAD = "\uD83D\uDCE5"; // 📥
--- src/main/java/com/jagrosh/jmusicbot/commands/owner/DebugCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/DebugCmd.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 John Grosh <john.a.grosh@gmail.com>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.jagrosh.jmusicbot.commands.owner;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
@@ -25,10 +11,7 @@ import net.dv8tion.jda.api.JDAInfo;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.ChannelType;
 
-/**
- *
- * @author John Grosh (john.a.grosh@gmail.com)
- */
+
 public class DebugCmd extends OwnerCommand 
 {
     private final static String[] PROPERTIES = {"java.version", "java.vm.name", "java.vm.specification.version", 
